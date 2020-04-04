16625b4e8777e4f7e95341a8c81df860ab5fd5ce
zal-lex <leshik89@gmail.com>
2020-04-04 14:20:29 +0300

    Add test of validation uniqueness of question.text

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,9 +11,11 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
 
   context 'validations check' do
+    subject { FactoryBot.create(:question) }
 
     it { should validate_presence_of :text }
     it { should validate_presence_of :level }
+    it { should validate_uniqueness_of :text }
 
     it { should validate_inclusion_of(:level).in_range(0..14) }
 
