1b8f9d963db6041d28594ea565e7461257bde1c9
zal-lex <leshik89@gmail.com>
2020-04-08 23:43:51 +0300

    Fix deprecation error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,21 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
+
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
