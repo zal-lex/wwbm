70c88e62fe79d22224287181b3d26af634ddd3d2
zal-lex <leshik89@gmail.com>
2020-04-09 17:46:18 +0300

    Add tests for Anonymous user

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -16,15 +16,50 @@ RSpec.describe GamesController, type: :controller do
   let(:game_w_questions) { FactoryBot.create(:game_with_questions, user: user) }
 
   # группа тестов для незалогиненного юзера (Анонимус)
-  context 'Anon' do
-    # из экшена show анона посылаем
-    it 'kick from #show' do
-      # вызываем экшен
-      get :show, id: game_w_questions.id
-      # проверяем ответ
+  shared_examples 'not authorised user' do
+    # проверяем ответ, должен быть не 200 ОК
+    it 'response not 200 OK' do
       expect(response.status).not_to eq(200) # статус не 200 ОК
+    end
+    # devise должен отправить на логин
+    it 'redirect to registration' do
       expect(response).to redirect_to(new_user_session_path) # devise должен отправить на логин
+    end
+    # во flash должен быть прописана ошибка
+    it 'flash has error message' do
       expect(flash[:alert]).to be # во flash должен быть прописана ошибка
+    end
+  end
+
+  context 'Anonymous user' do
+    context '#show' do
+      before(:example) { get :show, id: game_w_questions.id }
+      # из экшена show анона посылаем
+      it_behaves_like 'not authorised user'
+    end
+
+    context '#create' do
+      before(:example) { post :create, id: game_w_questions.id }
+      # из экшена create анона посылаем
+      it_behaves_like 'not authorised user'
+    end
+
+    context '#answer' do
+      before(:example) { put :answer, id: game_w_questions.id }
+      # из экшена answer анона посылаем
+      it_behaves_like 'not authorised user'
+    end
+
+    context '#take_money' do
+      before(:example) { put :take_money, id: game_w_questions.id }
+      # из экшена take_money анона посылаем
+      it_behaves_like 'not authorised user'
+    end
+
+    context '#help' do
+      before(:example) { put :help, id: game_w_questions.id }
+      # из экшена help анона посылаем
+      it_behaves_like 'not authorised user'
     end
   end
 
@@ -49,6 +84,22 @@ RSpec.describe GamesController, type: :controller do
       expect(flash[:notice]).to be
     end
 
+    # юзер пытается создать новую игру, не закончив старую
+    it 'try to create second game' do
+      # убедились что есть игра в работе
+      expect(game_w_questions.finished?).to be false
+
+      # отправляем запрос на создание, убеждаемся что новых Game не создалось
+      expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game) # вытаскиваем из контроллера поле @game
+      expect(game).to be nil
+
+      # и редирект на страницу старой игры
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
+    end
+
     # юзер видит свою игру
     it '#show game' do
       get :show, id: game_w_questions.id
@@ -58,6 +109,37 @@ RSpec.describe GamesController, type: :controller do
 
       expect(response.status).to eq(200) # должен быть ответ HTTP 200
       expect(response).to render_template('show') # и отрендерить шаблон show
+    end
+
+    # юзер не видит чужую игру
+    it '#show alien game' do
+      # создаем новую игру, юзер не прописан, будет создан фабрикой новый
+      alien_game = FactoryBot.create(:game_with_questions)
+
+      # пробуем зайти на эту игру текущий залогиненным user
+      get :show, id: alien_game.id
+
+      expect(response.status).not_to eq(200) # статус не 200 ОК
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be # во flash должен быть прописана ошибка
+    end
+
+    # юзер берет деньги
+    it 'takes money' do
+      # вручную поднимем уровень вопроса до выигрыша 200
+      game_w_questions.update_attribute(:current_level, 2)
+
+      put :take_money, id: game_w_questions.id
+      game = assigns(:game)
+      expect(game.finished?).to be true
+      expect(game.prize).to eq(200)
+
+      # пользователь изменился в базе, надо в коде перезагрузить!
+      user.reload
+      expect(user.balance).to eq(200)
+
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:warning]).to be
     end
 
     # юзер отвечает на игру корректно - игра продолжается
