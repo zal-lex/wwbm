2c08c77031be0d6c6bf08316584a471cec9497a6
zal-lex <leshik89@gmail.com>
2020-04-11 17:06:56 +0300

    Fix bug with user authorization

--- spec/views/user/show.html.erb_spec.rb
+++ spec/views/user/show.html.erb_spec.rb
@@ -3,53 +3,67 @@ require 'rails_helper'
 # Тест на шаблон users/show.html.erb
 
 RSpec.describe 'users/show', type: :view do
-  # Перед каждым шагом мы пропишем в переменную @user самого пользователя
-  # и застабим вывод шаблона для игр
   describe 'renders the players game partial and user name' do
-    before(:example) do
-      assign(:user, FactoryBot.build_stubbed(:user, name: 'James Bond'))
-      # stub_template 'users/_game.html.erb' => 'User game goes here'
-      assign(:games, [
-        FactoryBot.build_stubbed(:game, id: 15, created_at: Time.parse('2020.04.09, 13:00'), current_level: 10, prize: 1000),
-        FactoryBot.build_stubbed(:game, id: 10, created_at: Time.parse('2020.04.03, 16:00'), current_level: 11, prize: 32000)
-      ])
-    end
-
+    # объявим в shared_example общие проверки для всех контекстов
     shared_examples 'user template' do
       # Проверяем, что шаблон выводит имя игрока
       it 'player name' do
-        render
         expect(rendered).to have_content 'James Bond'
       end
 
       # Проверяем, что шаблон выводит паршиал
       # it 'renders partial' do
-        # expect(rendered).to have_content 'User game goes here'
+      #   expect(rendered).to have_content 'User game goes here'
       # end
+
       it 'renders player balances' do
-        render
         expect(rendered).to match /1 000.*32 000/m
       end
     end
 
+    # Проверка в случае не залогиненого пользователя
     context 'Anonymous user' do
-      it 'anonim have no link to change name or password' do
+      # Создадим пользователя и пару игр для него
+      before(:example) do
+        user = assign(:user, FactoryBot.create(:user, name: 'James Bond'))
+        # stub_template 'users/_game.html.erb' => 'User game goes here'
+        assign(:games, [
+          FactoryBot.build_stubbed(:game, id: 15, created_at: Time.parse('2020.04.09, 13:00'), current_level: 10, prize: 1000),
+          FactoryBot.build_stubbed(:game, id: 10, created_at: Time.parse('2020.04.03, 16:00'), current_level: 11, prize: 32000)
+        ])
         render
+      end
+
+      # Проверяем, что у анонима не выводится ссылка на смену логина и пароля
+      it 'anonim have no link to change name or password' do
         expect(rendered).not_to have_content 'Сменить имя и пароль'
       end
 
+      # Проверка вывода имени пользователя и отрисовки паршиала
       it_behaves_like 'user template'
     end
 
+    # Проверяем случай, когда пользователь залогинился
     context 'Authorized user' do
-      it 'authorized user have link to change name or password' do
-        user = FactoryBot.build_stubbed(:user, name: 'James Bond')
+      # Создадим пользователя и пару игр для него, авторизуем пользователя
+      before(:example) do
+        user = assign(:user, FactoryBot.create(:user, name: 'James Bond'))
+        # stub_template 'users/_game.html.erb' => 'User game goes here'
+        assign(:games, [
+          FactoryBot.build_stubbed(:game, id: 15, created_at: Time.parse('2020.04.09, 13:00'), current_level: 10, prize: 1000),
+          FactoryBot.build_stubbed(:game, id: 10, created_at: Time.parse('2020.04.03, 16:00'), current_level: 11, prize: 32000)
+        ])
         sign_in user
-        assign(:user, user)
+
         render
+      end
+
+      # На странице должна появиться ссылка на смену логина и пароля
+      it 'authorized user have link to change name or password' do
         expect(rendered).to have_content 'Сменить имя и пароль'
       end
 
+      # Проверка вывода имени пользователя и отрисовки паршиала
       it_behaves_like 'user template'
     end
 
